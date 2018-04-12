8153622e812eb9fba88ac39fe3eb616f93ac9497
KRAITcoin <Kraitcoin@gmail.com>
2018-04-12 15:42:59 +0300

    Revert "1"
    This reverts commit 8a0bfe216452d69840403d8d3e7e3cde37b733ef.

--- src/test/torcontrol_tests.cpp
+++ src/test/torcontrol_tests.cpp
@@ -21,8 +21,8 @@ BOOST_AUTO_TEST_CASE(util_SplitTorReplyLine)
 {
     // Data we should receive during normal usage
     CheckSplitTorReplyLine(
-        "PROTOCOLINFO kraitERSION",
-        "PROTOCOLINFO", "kraitERSION");
+        "PROTOCOLINFO KRAITERSION",
+        "PROTOCOLINFO", "KRAITERSION");
     CheckSplitTorReplyLine(
         "AUTH METHODS=COOKIE,SAFECOOKIE COOKIEFILE=\"/home/x/.tor/control_auth_cookie\"",
         "AUTH", "METHODS=COOKIE,SAFECOOKIE COOKIEFILE=\"/home/x/.tor/control_auth_cookie\"");
